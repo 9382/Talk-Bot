d7e537833791c1250f2d9dd52a503d07f2633077
9382 <omnomaidan@gmail.com>
2021-09-15 16:47:42 +0100

    More module loading changes
    mostly chucked it in a function and went "yeah ok, why not, now i can ##d it"

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,16 +1,22 @@
 # Handler for the modules folder
 # __import__ isnt friendly with sub-dirs, fromfile makes no sense, and ive got no wifi to figure out how, so exec it is
 import os
-for fname in os.listdir("modules"):
-    if not fname.endswith(".py"):
-        continue
-    if not os.path.isfile("modules/"+fname):
-        continue
-    fname = fname[:-3]
-    if fname == "__main__":
-        continue
-    try:
-        exec(f"from modules import {fname}")
-    except Exception as exc:
-        print("[Modules]",exc)
+def loadModules(origin=None):
+    print("Loading modules origin=",origin)
+    for fname in os.listdir("modules"):
+        if not fname.endswith(".py"):
+            continue
+        if not os.path.isfile("modules/"+fname):
+            continue
+        fname = fname[:-3]
+        if fname == "__main__":
+            continue
+        try:
+            exec(f"from modules import {fname}")
+        except Exception as exc:
+            print("[Modules]",fname,"failed:",exc)
+async def loadModulesAsync(msg,args):
+    loadModules("CMD -> "+msg.author.name)
+addCommand("d -reload modules",loadModulesAsync,0,"",{},None,"dev")
+loadModules("Import")
 print("Module importer finished")
